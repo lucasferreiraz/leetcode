a44a5b3def8c1210c490dbe6b2e8e3f5daf3a52d
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-05-31 20:07:28 -0300

    change: solution 217 - reducing time complexity

--- src/main/java/com/dsa/array/ContainsDuplicate.java
+++ src/main/java/com/dsa/array/ContainsDuplicate.java
@@ -1,17 +1,24 @@
 package com.dsa.array;
 
+import java.util.HashSet;
+
 public class ContainsDuplicate {
     
     public boolean containsDuplicate(int numbers[]) {
         int arrayLength = numbers.length;
-        
+
+        HashSet<Integer> numbersSet = new HashSet<>();
+
         if(arrayLength == 0)
             return false;
 
-        for(int i = 0; i < arrayLength; i++)
-            for(int j = i + 1; j < arrayLength; j++)
-                if(numbers[i]==numbers[j])
-                    return true;   
+        for(int number : numbers){
+            boolean existNumberInSet = numbersSet.contains(number);
+            if(existNumberInSet)
+                return true;
+            
+            numbersSet.add(number);
+        }
 
         return false;
     }
